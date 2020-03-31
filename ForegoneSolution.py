ddeeb0906ddb9bca892f5e5f4dad2cdfa8049562
psk43 <p.43saikrishna@gmail.com>
2020-04-01 01:08:15 +0530

    Update ForegoneSolution.py

--- ForegoneSolution.py
+++ ForegoneSolution.py
@@ -1,10 +1,12 @@
 T = int(input())
+Q=0
 while(T>0):
+    Q=Q+1
+    T=T-1
     N = str(input())
     A = B = 0
     for i in range(0,len(N)):
         if(N[i]=='4'):
-            B = B+10**i
+            B = B+10**(len(N)-i-1)
     A = int(N) - B   
-    print("Case #{}: {} {}".format(T,A,B))
-    T = T-1
+    print("Case #{}: {} {}".format(Q,A,B))
